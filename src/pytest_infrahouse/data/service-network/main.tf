bf3b38fe77f5ddbea833db9e4b6e106b643cf812
Oleksandr Kuzminskyi <1763754+akuzminsky@users.noreply.github.com>
2025-03-06 13:06:34 -0800

    service network dns (#7)
    * Enable DNS support in service network
    
    Needed to fix DNS lookups for EFS.
    
    * Bump version: 0.6.0 → 0.6.1

--- src/pytest_infrahouse/data/service-network/main.tf
+++ src/pytest_infrahouse/data/service-network/main.tf
@@ -4,6 +4,8 @@ module "service-network" {
   service_name          = "service-network"
   vpc_cidr_block        = "10.1.0.0/16"
   management_cidr_block = "10.1.0.0/16"
+  enable_dns_hostnames  = true
+  enable_dns_support    = true
   subnets = [
     {
       cidr                    = "10.1.0.0/24"
