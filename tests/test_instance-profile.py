519585515c8fd489993bbcaa7b11ab7da7fdf142
Oleksandr Kuzminskyi <aleks@infrahouse.com>
2025-10-11 10:50:02 -0700

    Fix test test_instance_profile
    Both instance role and name are random now. Adjust the test accordingly.

--- tests/test_instance-profile.py
+++ tests/test_instance-profile.py
@@ -12,7 +12,9 @@ def test_instance_profile(test_role_arn, aws_region, keep_after, request):
             fp.write(f'role_arn = "{test_role_arn}"\n')
 
     with terraform_apply(module_path, destroy_after=not keep_after) as tf_output:
-        assert tf_output["instance_profile_name"]["value"] == "website-pod-profile"
+        assert tf_output["instance_profile_name"]["value"].startswith(
+            "website-pod-profile"
+        )
         assert tf_output["instance_role_name"]["value"].startswith(
             "website-pod-profile"
         )
