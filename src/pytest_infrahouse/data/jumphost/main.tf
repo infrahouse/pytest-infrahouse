501f3dac50996bb95a7a766041915bc78eb93cdd
Oleksandr Kuzminskyi <aleks@infrahouse.com>
2025-10-11 13:41:53 -0700

    Ensure random EFS token - for concurrent jumphosts

--- src/pytest_infrahouse/data/jumphost/main.tf
+++ src/pytest_infrahouse/data/jumphost/main.tf
@@ -1,8 +1,14 @@
+resource "random_string" "suffix" {
+  length  = 8
+  special = false
+}
+
 module "jumphost" {
   source                   = "registry.infrahouse.com/infrahouse/jumphost/aws"
-  version                  = "4.2.0"
+  version                  = "4.3.0"
   environment              = var.environment
   keypair_name             = aws_key_pair.jumphost.key_name
+  efs_creation_token       = "jumphost-home-${random_string.suffix.result}"
   route53_zone_id          = var.test_zone_id
   subnet_ids               = var.subnet_private_ids
   nlb_subnet_ids           = var.subnet_public_ids
