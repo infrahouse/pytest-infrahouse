a84c8b0a7c93faeb6fcb189ebbc23928de4cde73
Oleksandr Kuzminskyi <aleks@infrahouse.com>
2025-12-05 05:51:19 -0800

    fix: resolve circular import in package initialization
    Move shared constants (LOG, DEFAULT_PROGRESS_INTERVAL) before terraform import in __init__.py to prevent circular import error. This fixes the ImportError that occurred when terraform.py tried to import constants from a partially initialized module.

--- src/pytest_infrahouse/__init__.py
+++ src/pytest_infrahouse/__init__.py
@@ -1,9 +1,9 @@
 import logging
 
-from .terraform import terraform_apply
-
 __version__ = "0.21.0"
 
 # Shared constants
 LOG = logging.getLogger(__name__)
 DEFAULT_PROGRESS_INTERVAL = 10
+
+from .terraform import terraform_apply
